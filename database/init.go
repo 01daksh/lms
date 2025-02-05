a959b4b3566e805f08356acde7db9fedad6e3c82
daksh01geniemode <daksh.chaudhary@geniemode.com>
2025-02-05 23:04:23 +0530

    .env enviromnent added

--- database/init.go
+++ database/init.go
@@ -1,10 +1,11 @@
 package database
 
 import (
-	"log"
-
 	"lms/models"
+	"log"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -12,8 +13,19 @@ import (
 var DB *gorm.DB
 
 func InitDatabase() {
-	var err error
-	dsn := "host=localhost user=postgres password=abc.dak01 dbname=library_db port=5432 sslmode=disable"
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	dbHost := os.Getenv("DB_HOST")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbPort := os.Getenv("DB_PORT")
+	dbSSLMode := os.Getenv("DB_SSLMODE")
+
+	// strings could be concatenated
+	dsn := "host=" + dbHost + " user=" + dbUser + " password=" + dbPassword + " dbname=" + dbName + " port=" + dbPort + " sslmode=" + dbSSLMode
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
